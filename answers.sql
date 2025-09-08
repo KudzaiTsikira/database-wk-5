c120e554f8b25aac521ba06e71f9bbc67b73c14d
KudzaiTsikira <84228129+KudzaiTsikira@users.noreply.github.com>
2025-09-08 12:38:15 +0200

    Update answers.sql

---
+++ answers.sql
@@ -0,0 +1,8 @@
+ALTER TABLE customers
+DROP INDEX IdxPhone;
+
+CREATE USER bob@localhost IDENTIFIED BY 'S$cu3r3!';
+
+GRANT INSERT ON salesDB TO bob@localhost;
+
+SET PASSWORD FOR bob@localhost = 'P$55!23';
